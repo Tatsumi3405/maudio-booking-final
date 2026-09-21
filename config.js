5198bc0ee82f6bd43a85bbbb27867d06c66adad0
Tatsumi3405 <rommelbaro4@gmail.com>
2026-09-22 04:57:14 +0800

    Fix admin login: add admins row + RLS select policy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window._env_ = {
   SUPABASE_URL:      'https://ponrkrqsmktdgclpuvyd.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvbnJrcnFzbWt0ZGdjcHV2eWQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3NDE4MjYyOCwiZXhwIjoyMDg5NzU4NjI4fQ.uSKHTHx84yIac1QJAcpwOtZMq1FImaDjAhyppakvWvQ',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvbnJrcnFzbWt0ZGdjbHB1dnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODI2MjgsImV4cCI6MjA4OTc1ODYyOH0.uSKHTHx84yIac1QJAcpwOtZMq1FImaDjAhyppakvWvQ  ',
   WORKER_URL:        'https://maudio-booking-webhook.rommelbaro4.workers.dev'
 };
